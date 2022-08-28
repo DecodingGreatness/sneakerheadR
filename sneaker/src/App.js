98f08d3df0243a0adf7d0664a86a8f28c261d1e2
Edward Ho <pythoned77@gmail.com>
2022-08-28 20:13:11 +0800

    added css to searchbar and app

--- sneaker/src/App.js
+++ sneaker/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import SearchBar from "./Components/SearchBar";
+import SneakerData from "./Data.json";
 
 function App() {
   return (
     <div className="App">
-      <SearchBar placeholder="Enter a Sneaker Name..." />
+      <SearchBar placeholder="Enter a Sneaker Name..." data={SneakerData} />
     </div>
   );
 }
